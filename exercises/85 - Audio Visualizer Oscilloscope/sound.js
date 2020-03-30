d9a2e66650aad40324ccb86a915bfab370c62410
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-03-30 18:17:04 -0400

    creates an audio visualizer

--- exercises/85 - Audio Visualizer Oscilloscope/sound.js
+++ exercises/85 - Audio Visualizer Oscilloscope/sound.js
@@ -1,16 +1,104 @@
-import { hslToRgb } from './utils';
+/* eslint-disable */
+// import { hslToRgb } from "./utils";
+
+// const WIDTH = 1500;
+// const HEIGHT = 1500;
+// const canvas = document.querySelector("canvas");
+// const ctx = canvas.getContext("2d");
+// canvas.width = WIDTH;
+// canvas.height = HEIGHT;
+// let analyzer;
+// let bufferLength;
+
+// function handleError(err) {
+//   console.log("You must give access to your mic to proceed");
+// }
+
+// async function getAudio() {
+//   // get access to user's microphone
+//   const stream = await navigator.mediaDevices
+//     .getUserMedia({ audio: true })
+//     .catch(handleError);
+//   const audioCtx = new AudioContext();
+//   analyzer = audioCtx.createAnalyser();
+//   const source = audioCtx.createMediaStreamSource(stream);
+//   source.connect(analyzer);
+//   // How much data should we collect?
+//   analyzer.fftSize = 2 ** 10;
+//   // pull the data off the audio
+//   // how many pieces of data are there?
+//   bufferLength = analyzer.frequencyBinCount;
+//   const timeData = new Uint8Array(bufferLength);
+//   const frequencyData = new Uint8Array(bufferLength);
+//   console.log(timeData);
+//   console.log(frequencyData);
+//   drawTimeData(timeData);
+//   drawFrequency(frequencyData);
+// }
+
+// function drawTimeData(timeData) {
+//   // inject the time data into our timedata arrray
+//   analyzer.getByteTimeDomainData(timeData);
+//   // we have the data, now to visualize!!!!
+//   // 1. Clear the canvas
+//   ctx.clearRect(0, 0, WIDTH, HEIGHT);
+//   // 2. set up some canvas drawing
+//   ctx.lineWidth = 10;
+//   ctx.strokeStyle = "#ffc600";
+//   ctx.beginPath();
+//   const sliceWidth = WIDTH / bufferLength;
+//   let x = 0;
+//   timeData.forEach((data, i) => {
+//     const v = data / 128;
+//     const y = (v * HEIGHT) / 2;
+//     // draw our lines
+//     if (i === 0) {
+//       ctx.moveTo(x, y);
+//     } else {
+//       ctx.lineTo(x, y);
+//     }
+//     x += sliceWidth;
+//   });
+
+//   ctx.stroke();
+//   // call itself as soon as possible
+//   requestAnimationFrame(() => drawTimeData(timeData));
+// }
+
+// function drawFrequency(frequencyData) {
+//   // get the frequency data into arr
+//   analyzer.getByteFrequencyData(frequencyData);
+//   // figure out the bar width
+//   const barWidth = (WIDTH / bufferLength) * 2.5;
+//   let x = 0;
+//   frequencyData.forEach(amount => {
+//     // 0 to 255
+//     const percent = amount / 255;
+//     const [h, s, l] = [360 / (percent * 360), 0.5, 0.75];
+//     const barHeight = (HEIGHT * percent) / 2;
+//     // CONVERT color to HSL TODO
+//     const [r, g, b] = hslToRgb(h, s, l);
+//     ctx.fillStyle = `rgb(${r},${g},${b})`;
+//     ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+//     x += barWidth + 1;
+//   });
+//   requestAnimationFrame(() => drawFrequency(frequencyData));
+// }
+
+// getAudio();
+import { hslToRgb } from "./utils";
 
 const WIDTH = 1500;
 const HEIGHT = 1500;
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 let analyzer;
 let bufferLength;
 
 function handleError(err) {
-  console.log('You must give access to your mic in order to proceed');
+  console.log("You must give access to your mic in order to proceed");
 }
 
 async function getAudio() {
@@ -40,7 +128,7 @@ function drawTimeData(timeData) {
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
   // 2. setup some canvas drawing
   ctx.lineWidth = 10;
-  ctx.strokeStyle = '#ffc600';
+  ctx.strokeStyle = "#ffc600";
   ctx.beginPath();
   const sliceWidth = WIDTH / bufferLength;
   let x = 0;
