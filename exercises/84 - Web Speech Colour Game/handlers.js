1a10ab316f0d4bebd38d6afbaa5c4892a02e5714
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-03-30 17:33:21 -0400

    creates a color picker based on the users voice

--- exercises/84 - Web Speech Colour Game/handlers.js
+++ exercises/84 - Web Speech Colour Game/handlers.js
@@ -1,4 +1,29 @@
-import { isValidColor } from './colors';
+// import { isValidColor } from "./colors";
+
+// function logWords(results) {
+//   console.log(results[results.length - 1][0].transcript);
+// }
+
+// export function handleResult({ results }) {
+//   logWords(results);
+//   const words = results[results.length - 1][0].transcript;
+//   console.log(words);
+//   // lowercase everything
+//   let color = words.toLowerCase();
+//   // strip any spaces out
+//   color = color.replace(/\s/g, "");
+//   // check if it is a valid color
+//   if (!isValidColor(color)) return;
+//   // if it is, show the UI
+//   const colorSpan = document.querySelector(`.${color}`);
+//   colorSpan.classList.add("got");
+//   // const colorSpan = document.getElementsByClassName(color)
+//   console.log(color);
+//   // change the background color
+//   document.body.style.backgroundColor = color;
+// }
+
+import { isValidColor } from "./colors";
 
 function logWords(results) {
   // console.log(results[results.length - 1][0].transcript);
@@ -10,14 +35,14 @@ export function handleResult({ results }) {
   // lowercase everything
   let color = words.toLowerCase();
   // strip any spaces out
-  color = color.replace(/\s/g, '');
+  color = color.replace(/\s/g, "");
   // check if it is a valid colour
   if (!isValidColor(color)) return; // thats all folks
   // if it is, then show the UI for that
   const colorSpan = document.querySelector(`.${color}`);
-  colorSpan.classList.add('got');
+  colorSpan.classList.add("got");
   console.log(colorSpan);
-  console.log('This is a valid color!');
+  console.log("This is a valid color!");
   console.log(color);
   // change the background color
   document.body.style.backgroundColor = color;
